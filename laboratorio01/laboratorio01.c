5038e67cce40d8237579a24f808eb2883d472a34
Leonardo Rocha <58191102+leonardosantosrocha@users.noreply.github.com>
2022-02-11 08:46:38 -0300

    Laboratório 01 - Hello World

---
+++ laboratorio01/laboratorio01.c
@@ -0,0 +1,15 @@
+// Laboratório 01 - Hello World
+
+// Nome: Leonardo Rocha
+// T.I.A. 32092261
+// Nome: Vitor Uema
+// T.I.A. 32028555
+// Nome: Talyta Scaramuzzo 
+// T.I.A. 32079915
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world!");
+    return 0;
+}
