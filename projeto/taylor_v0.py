05fd3c8d2b3e8ad371543c9d2c36b0cac852c2e1
Leonardo Rocha <58191102+leonardosantosrocha@users.noreply.github.com>
2022-05-02 19:17:05 -0300

    Delete taylor_v0.py

--- projeto/taylor_v0.py
+++
@@ -1,8 +0,0 @@
-# 1. Crie um programa para calcular a fórmula S = (1/1) + (1/2) + (1/3) + ... (1/T).
-def taylor(t:int) -> float:
-    sum = 0
-    for i in range(1, t):
-        sum = sum + (1/i)
-    return sum
- 
-print(taylor(10))
