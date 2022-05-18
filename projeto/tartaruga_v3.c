8ce25010ac7d161177d8653ee5d8a4ce99fbf904
Leonardo Rocha <58191102+leonardosantosrocha@users.noreply.github.com>
2022-05-17 21:28:42 -0300

    Projeto - Tartaruga V3
    Implementação final da V3 do Projeto Tartaruga.

--- projeto/tartaruga_v3.c
+++ projeto/tartaruga_v3.c
@@ -2,31 +2,36 @@
 #include <stdlib.h>
 #include <omp.h>
 
-#define t 10000000
-
-int thread_count = 2;
+int thread_count = 6;
 long double global_sum = 0.0;
+long double t = 100000000000;
 
-void tartaruga()
+long double tartaruga()
 {
 	int thread_id = omp_get_thread_num();
-    long double i, min, max, local_sum = 0;
+   	long double i, min, max, local_sum = 0.0;
 	
 	min = 1 + (t / (thread_count)) * thread_id;
 	max = (t / (thread_count)) + (t / (thread_count)) * thread_id;
 
-	
-    for (i = min; i <= max ; i++) {
+    	for (i = min; i <= max ; i++) 
+	{
 		local_sum += (1 / i);
-        printf("%Lf %Lf\n", i, local_sum);
 	}
-    #pragma omp critical
-    global_sum += global_sum + local_sum;
+	printf("i = %Lf | local_sum = %Lf\n", i, local_sum);
+	return local_sum;
 }
+
 int main()
 {
-    # pragma omp parallel num_threads(thread_count)
-    tartaruga();
-    printf("%Lf\n", global_sum);
+	long double local_sum = 0.0;
+
+	# pragma omp parallel num_threads(thread_count)
+	local_sum += tartaruga();
+
+	#pragma omp critical
+	global_sum += local_sum;
+
+	printf("%Lf\n", global_sum);
 	return 0;
 }
